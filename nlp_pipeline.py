b0895d35f4403e8f24645d4d59e5c4920005652d
sbangapadang22 <sbangapadang@mines.edu>
2024-04-09 21:33:38 -0600

    expanded pipeline with additional NLP tasks

--- nlp_pipeline.py
+++ nlp_pipeline.py
@@ -1,13 +1,57 @@
 import nltk
 from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
+from nltk.stem import WordNetLemmatizer
+from nltk.sentiment import SentimentIntensityAnalyzer
 
 nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+nltk.download('vader_lexicon')
 
 def tokenize_text(text):
     return word_tokenize(text)
 
-# token function example - splits into list
-if __name__ == "__main__":
-    text = "Sample text for tokenization."
+def pos_tag_text(tokens):
+    return pos_tag(tokens)
+
+def lemmatize_text(tokens):
+    lemmatizer = WordNetLemmatizer()
+    return [lemmatizer.lemmatize(token) for token in tokens]
+
+def analyze_sentiment(text):
+    sia = SentimentIntensityAnalyzer()
+    sentiment_scores = sia.polarity_scores(text)
+    return sentiment_scores
+
+def named_entity_recognition(tokens):
+    # TODO: function for NER
+    # maybe use NLTK's named entity chunker or other libraries like spaCy for NER
+    return []
+
+def text_classification(text):
+    # TODO: function for text classification
+    # maybeuse NLTK's Naive Bayes classifier or other machine learning algorithms for classification
+    return []
+
+def process_text(text):
     tokens = tokenize_text(text)
-    print(tokens)
+    pos_tags = pos_tag_text(tokens)
+    lemmas = lemmatize_text(tokens)
+    sentiment = analyze_sentiment(text)
+    named_entities = named_entity_recognition(tokens)
+    classification = text_classification(text)
+
+    return {
+        'tokens': tokens,
+        'pos_tags': pos_tags,
+        'lemmas': lemmas,
+        'sentiment': sentiment,
+        'named_entities': named_entities,
+        'classification': classification
+    }
+
+if __name__ == "__main__":
+    text = "The quick brown fox jumps over the lazy dog. It was a fantastic experience!"
+    result = process_text(text)
+    print(result)
